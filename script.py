130c6bf3439570f6d4e736c3818e3b219e4b8af2
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-03-20 21:38:03 +0000

    created script

--- script.py
+++ script.py
@@ -2,10 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def transkribus ():
-
-    driver = webdriver.Chrome()
-    driver.get("https://readcoop.eu/model/print-multi-language-danish-dutch-german-finnish-french-latin-swedish/")
-    time.sleep(20)
+driver = webdriver.Chrome()
+driver.get("https://readcoop.eu/model/print-multi-language-danish-dutch-german-finnish-french-latin-swedish/")
+time.sleep(20)
     
-transkribus ()
