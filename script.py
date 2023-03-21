9ebdec5aca875f76a262fd566d244be92f2e852b
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-03-21 15:26:37 +0000

    modified script

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
+service = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://readcoop.eu/model/print-multi-language-danish-dutch-german-finnish-french-latin-swedish/")
 time.sleep(20)
     
