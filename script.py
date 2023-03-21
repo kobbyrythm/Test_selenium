23739b19864239f3a94a4f93a5079a078b686750
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2023-03-21 10:18:51 +0000

    modified script

--- script.py
+++ script.py
@@ -1,8 +1,15 @@
+from os import environ
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 
-driver = webdriver.Chrome()
+chrome_service = Service(environ['CHROMEWEBDRIVER'])
+chrome_options = Options()
+for option in ['--headless','--disable-gpu','--window-size=1920,1200','--ignore-certificate-errors','--disable-extensions','--no-sandbox','--disable-dev-shm-usage']:
+    chrome_options.add_argument(option)
+driver = webdriver.Chrome(service = chrome_service,options = chrome_options)
+
 driver.get("https://readcoop.eu/model/print-multi-language-danish-dutch-german-finnish-french-latin-swedish/")
 time.sleep(20)
     
